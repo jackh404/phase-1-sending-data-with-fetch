85119fb08fae574d7831f63a70aa8d5382d3b821
Jack Henza <jackhenza@gmail.com>
2023-10-06 15:41:11 -0400

    creates submitData

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+const formData = {
+    dogName: "Rocket",
+    dogBreed: "Husky"
+}
+
+function configObj(name,email) {
+    return {
+        method: 'POST',
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    }
+}
+
+function submitData(name,email){
+    return fetch("http://localhost:3000/users",configObj(name,email))
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+            const userNum = document.createElement('h4')
+            userNum.textContent = data.id
+            document.body.append(userNum)
+        })
+        .catch(error =>{
+            alert("Whoopsie poopsie! That's an error!")
+            document.body.append(error.message)
+        })
+}
